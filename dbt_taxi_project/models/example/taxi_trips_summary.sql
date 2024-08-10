df38b1c8d3e73ffaf119a85a4e845d69b704eb20
Yee Teck Chong <ytchong222@google.com>
2024-08-10 19:52:22 +0800

    test dbt file

--- dbt_taxi_project/models/example/taxi_trips_summary.sql
+++ dbt_taxi_project/models/example/taxi_trips_summary.sql
@@ -1,38 +1,43 @@
-
 {{ config(materialized='table') }}
 
 WITH raw_data AS (
     SELECT
         taxi_id,
-		company,
+        company,
         trip_start_timestamp,
         trip_end_timestamp,
         fare,
         tips,
         trip_miles,
-		payment_type,
-		extras,
-		tolls,
-        case when trip_seconds is null then abs(TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND)) end as trip_seconds
+        payment_type,
+        extras,
+        tolls,
+        CASE 
+            WHEN trip_seconds IS NULL THEN 
+                ABS(TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND))
+            ELSE 
+                trip_seconds
+        END AS trip_seconds
     FROM
         chicago_taxi_trips.taxi_trips   -- referencing the raw table in BigQuery taxi_trips
 )
 
 SELECT
-    (FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp)) as tdate,
+    FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp) AS tdate,
     taxi_id,
-	company as taxi_company,
+    company AS taxi_company,
     COUNT(taxi_id) AS trip_count,
     SUM(fare) AS total_fare,
     SUM(tips) AS total_tips,
     SUM(trip_miles) AS total_miles,
     SUM(trip_seconds) AS total_seconds,
-	sum(extras) as total_extra_payment,
-	sum(tolls) as total_tolls,
-	payment_type
+    SUM(extras) AS total_extra_payment,
+    SUM(tolls) AS total_tolls,
+    payment_type
 FROM
     raw_data
 GROUP BY
-    taxi_id,company,(FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp)),payment_type
-
-
+    tdate,
+    taxi_id,
+    taxi_company,
+    payment_type
