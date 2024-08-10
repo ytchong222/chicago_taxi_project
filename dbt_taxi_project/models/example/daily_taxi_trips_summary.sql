ec7c2a2e14605e23aa59004138cf86efed513746
Yee Teck Chong <ytchong222@google.com>
2024-08-10 14:53:30 +0800

    test dbt file

--- dbt_taxi_project/models/example/daily_taxi_trips_summary.sql
+++ dbt_taxi_project/models/example/daily_taxi_trips_summary.sql
@@ -9,7 +9,8 @@ select tdate as tdate,
 	sum(total_fare) as total_fare, 
 	sum(total_tips) as total_tips,
 	SUM(total_miles) AS total_miles,
-	SUM(total_seconds) AS total_seconds
+	SUM(total_seconds) AS total_seconds,
+	sum(total_tolls) as total_tolls,
 	from  dbt_taxi_trips.taxi_trips_summary
 	where tdate is not null
 group by tdate
--- dbt_taxi_project/models/example/taxi_trips_summary.sql
+++ dbt_taxi_project/models/example/taxi_trips_summary.sql
@@ -4,12 +4,16 @@
 WITH raw_data AS (
     SELECT
         taxi_id,
+		company,
         trip_start_timestamp,
         trip_end_timestamp,
         fare,
         tips,
         trip_miles,
-        trip_seconds
+		payment_type,
+		extra,
+		tolls,
+        case when trip_seconds is null then abs(TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND)) end as trip_seconds
     FROM
         chicago_taxi_trips.taxi_trips   -- referencing the raw table in BigQuery taxi_trips
 )
@@ -17,12 +21,18 @@ WITH raw_data AS (
 SELECT
     (FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp)) as tdate,
     taxi_id,
+	company as taxi_company,
     COUNT(taxi_id) AS trip_count,
     SUM(fare) AS total_fare,
     SUM(tips) AS total_tips,
     SUM(trip_miles) AS total_miles,
-    SUM(trip_seconds) AS total_seconds
+    SUM(trip_seconds) AS total_seconds,
+	sum(extra) as total_extra_payment,
+	sum(tolls) as total_tolls,
+	payment_type
 FROM
     raw_data
 GROUP BY
-    taxi_id,(FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp))
+    taxi_id,company,(FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp)),payment_type
+
+
