b51b0595e55e04344ac1c78a1bc4474e8cc8a438
Yee Teck Chong <ytchong222@google.com>
2024-08-11 22:22:00 +0800

    new plugin file

--- plugins/custom_bigquery_plugin/__init__.py
+++ plugins/custom_bigquery_plugin/__init__.py
@@ -1,2 +1,13 @@
 # plugins/custom_bigquery_plugin/__init__.py
-from .bq_helpers import create_bq_dataset_if_not_exists, create_bq_table_if_not_exists, import_from_kaggle_to_bucket, run_load_data_to_bigquery
+# File: custom_bigquery_plugin/__init__.py
+from custom_bigquery_plugin.bq_helpers import (
+    create_bq_dataset_if_not_exists,
+    create_bq_table_if_not_exists,
+    import_from_kaggle_to_bucket,
+    run_load_data_to_bigquery
+)
+
+# plugins/custom_bigquery_plugin/__init__.py
+#import logging
+#logging.basicConfig(level=logging.INFO)
+#from airflow.plugins_manager import AirflowPlugin
