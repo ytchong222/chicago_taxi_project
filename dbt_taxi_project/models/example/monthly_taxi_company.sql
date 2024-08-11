71c11d13466700b3a241744dc76834aadbc4524c
Yee Teck Chong <ytchong222@google.com>
2024-08-11 18:11:52 +0800

    new dbt file

--- dbt_taxi_project/models/example/monthly_taxi_company.sql
+++ dbt_taxi_project/models/example/monthly_taxi_company.sql
@@ -3,19 +3,21 @@
 {{ config(materialized='table') }}
 
 
-select tdate as tdate,
+SELECT 
+    substring(tdate,1,7) as monthdate,
     taxi_company,
-	count(taxi_id) total_taxi,
-	sum(trip_count) total_trips,
-	sum(total_fare) as total_fare, 
-	sum(total_tips) as total_tips,
-	SUM(total_miles) AS total_miles,
-	SUM(total_seconds) AS total_seconds,
-	sum(total_tolls) as total_tolls,
-	from  dbt_taxi_trips.taxi_trips_summary
-group by tdate,taxi_company
-
-
+    COUNT(taxi_id) AS total_taxi,
+    SUM(trip_count) AS total_trips,
+    SUM(total_fare) AS total_fare,
+    SUM(total_tips) AS total_tips,
+    SUM(total_miles) AS total_miles,
+    SUM(total_seconds) AS total_seconds,
+    SUM(total_tolls) AS total_tolls
+FROM  
+    dbt_taxi_trips.taxi_trips_summary
+GROUP BY 
+    substring(tdate,1,7), 
+    taxi_company
 
 
 
