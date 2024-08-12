56c0da72350c2d368429cc8b5f77f200a2849512
Yee Teck Chong <ytchong222@google.com>
2024-08-12 22:28:15 +0800

    new dag file for schedule

--- dbt_taxi_project/models/example/top_100_overworkers.sql
+++ dbt_taxi_project/models/example/top_100_overworkers.sql
@@ -1,40 +1,21 @@
 -- models/top_100_overworker.sql
 {{ config(materialized='table') }}
  WITH top_100_overworker AS (
-   select   a.taxi_id,
-        a.trip_start_timestamp,
-        a.trip_end_timestamp,
-        a.trip_seconds,trip_duration,
-        LAG(a.trip_end_timestamp) OVER (PARTITION BY a.taxi_id ORDER BY a.trip_start_timestamp,a.trip_end_timestamp) AS prev_trip_end_timestamp
-     from(
+
         SELECT
             taxi_id,
             trip_start_timestamp,
-            case 
-            when trip_end_timestamp is null 
-                 then  
-                 TIMESTAMP_ADD(trip_start_timestamp, INTERVAL trip_seconds SECOND) 
-            else trip_end_timestamp end as trip_end_timestamp,
-            case 
-            when trip_end_timestamp is not null 
-                 then 
-                 TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND) end AS trip_duration,
-            CASE 
-            WHEN trip_end_timestamp IS NULL THEN 
-                trip_seconds 
-            ELSE 
-                0
-        END AS trip_seconds
+            trip_end_timestamp,
+            TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND) end AS trip_duration,
+             LAG(trip_end_timestamp) OVER (PARTITION BY taxi_id ORDER BY trip_start_timestamp,a.trip_end_timestamp) AS prev_trip_end_timestamp
     FROM
         `chicago_taxi_trips.taxi_trips`
-   
-         )a
- where a.trip_end_timestamp>=a.trip_start_timestamp
+ where trip_end_timestamp>=trip_start_timestamp
 )
 SELECT
     taxi_id,
     COUNT(*) AS total_trips,
-    SUM(ABS(trip_duration)+trip_seconds) / 3600 AS total_hours_trips, 
+    SUM(trip_duration) / 3600 AS total_hours_trips, 
     AVG(ABS(TIMESTAMP_DIFF(trip_start_timestamp, prev_trip_end_timestamp, HOUR)))  AS avg_break_hours
 FROM
     top_100_overworker
