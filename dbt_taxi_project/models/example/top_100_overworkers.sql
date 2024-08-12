ca498d3cef24634dffdb34005748feefe0f5fb12
Yee Teck Chong <ytchong222@google.com>
2024-08-12 22:37:34 +0800

    new dag file for schedule

--- dbt_taxi_project/models/example/top_100_overworkers.sql
+++ dbt_taxi_project/models/example/top_100_overworkers.sql
@@ -5,11 +5,11 @@
             taxi_id,
             trip_start_timestamp,
             trip_end_timestamp,
-            TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND) end AS trip_duration,
+            TIMESTAMP_DIFF(trip_end_timestamp, trip_start_timestamp, SECOND)  AS trip_duration,
              LAG(trip_end_timestamp) OVER (PARTITION BY taxi_id ORDER BY trip_start_timestamp,trip_end_timestamp) AS prev_trip_end_timestamp
     FROM
         `chicago_taxi_trips.taxi_trips`
- where trip_end_timestamp>=trip_start_timestamp
+ where trip_end_timestamp>=trip_start_timestamp 
 )
 SELECT
     taxi_id,
