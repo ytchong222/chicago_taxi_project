32cb71380c27833b3f2152fbda83eea32eb373d7
Yee Teck Chong <ytchong222@google.com>
2024-08-13 16:19:23 +0800

    dbt top 100 overworkers

--- dbt_taxi_project/models/example/trip_revenue.sql
+++ dbt_taxi_project/models/example/trip_revenue.sql
@@ -2,16 +2,8 @@
 
 WITH trip_revenue AS (
     SELECT
-        taxi_id,
         company,
-        COALESCE(CAST(pickup_community_area AS STRING), 'NA') AS pickup_community_area,
-        COALESCE(CAST(dropoff_community_area AS STRING), 'NA') AS dropoff_community_area,
-        trip_start_timestamp,
         trip_end_timestamp,
-        fare,
-        tips,
-        tolls,
-        extras,
         (fare + tips  + extras- tolls) AS total_revenue
     FROM
         `chicago_taxi_trips.taxi_trips`
@@ -22,21 +14,13 @@ WITH trip_revenue AS (
 SELECT
     FORMAT_TIMESTAMP('%Y-%m', trip_end_timestamp) as monthdate,
     company,
-    pickup_community_area,
-    dropoff_community_area,
     COUNT(*) AS total_trips,
     SUM(total_revenue) AS total_revenue_generated,
-    AVG(total_revenue) AS avg_revenue_per_trip,
-    SUM(fare) AS total_fare,
-    SUM(tips) AS total_tips,
-    SUM(tolls) AS total_tolls,
-    SUM(extras) AS total_extras
+    AVG(total_revenue) AS avg_revenue_per_trip
 FROM
     trip_revenue
 GROUP BY
     company,
-    pickup_community_area,
-    dropoff_community_area,
     monthdate
     
 ORDER BY
