434805eb1fcdac97e4b6d0abad5d5c9c5bb03973
Yee Teck Chong <ytchong222@google.com>
2024-08-13 17:18:10 +0800

    dbt top 100 overworkers

--- dbt_taxi_project/models/example/demand_pickup.sql
+++ dbt_taxi_project/models/example/demand_pickup.sql
@@ -1,15 +1,24 @@
 {{ config(materialized='table') }}
 
 SELECT
-  EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS day_of_week,
+   CASE 
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 1 THEN 'Sunday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 2 THEN 'Monday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 3 THEN 'Tuesday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 4 THEN 'Wednesday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 5 THEN 'Thursday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 6 THEN 'Friday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 7 THEN 'Saturday'
+    ELSE 'NA' 
+  END AS day_of_week,
   COALESCE(CAST(pickup_community_area AS STRING), 'NA') AS pickup_community_area,
   COUNT(*) AS number_of_trips
 FROM
   `chicago_taxi_trips.taxi_trips`
 WHERE
-        FORMAT_TIMESTAMP('%Y-%m-%d', trip_start_timestamp) BETWEEN '2023-06-01' AND '2024-01-02'
+        FORMAT_TIMESTAMP('%Y-%m-%d', trip_start_timestamp) BETWEEN '2023-10-01' AND '2024-01-02'
 GROUP BY
    pickup_community_area
 ORDER BY
   number_of_trips DESC
-LIMIT 100
+
--- dbt_taxi_project/models/example/peak_hours.sql
+++ dbt_taxi_project/models/example/peak_hours.sql
@@ -1,12 +1,25 @@
 {{ config(materialized='table') }}
-SELECT
-  EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS day_of_week,
+
+  SELECT
+  CASE 
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 1 THEN 'Sunday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 2 THEN 'Monday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 3 THEN 'Tuesday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 4 THEN 'Wednesday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 5 THEN 'Thursday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 6 THEN 'Friday'
+    WHEN EXTRACT(DAYOFWEEK FROM trip_start_timestamp) = 7 THEN 'Saturday'
+    ELSE 'NA' 
+  END AS day_of_week,
   EXTRACT(HOUR FROM trip_start_timestamp) AS hour_of_day,
   COUNT(*) AS number_of_trips
 FROM
   `chicago_taxi_trips.taxi_trips`
+WHERE
+   FORMAT_TIMESTAMP('%Y-%m-%d', trip_start_timestamp) BETWEEN '2023-10-01' AND '2024-01-02'
 GROUP BY
   day_of_week,
   hour_of_day
 ORDER BY
-  number_of_trips DESC 
+  number_of_trips DESC
+
