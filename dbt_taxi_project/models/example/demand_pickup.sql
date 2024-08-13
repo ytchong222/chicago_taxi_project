4f250b2102ed7acbd2452668a749aec65620b44d
Yee Teck Chong <ytchong222@google.com>
2024-08-13 17:06:17 +0800

    dbt top 100 overworkers

--- dbt_taxi_project/models/example/demand_pickup.sql
+++ dbt_taxi_project/models/example/demand_pickup.sql
@@ -1,10 +1,15 @@
+{{ config(materialized='table') }}
+
 SELECT
-  pickup_location,
+  EXTRACT(DAYOFWEEK FROM trip_start_timestamp) AS day_of_week,
+  COALESCE(CAST(pickup_community_area AS STRING), 'NA') AS pickup_community_area,
   COUNT(*) AS number_of_trips
 FROM
   `chicago_taxi_trips.taxi_trips`
+WHERE
+        FORMAT_TIMESTAMP('%Y-%m-%d', trip_start_timestamp) BETWEEN '2023-06-01' AND '2024-01-02'
 GROUP BY
-  pickup_location
+   pickup_community_area
 ORDER BY
   number_of_trips DESC
 LIMIT 100
