aa51e8590da51f1f37b65cb45dae7169fdd82364
Yee Teck Chong <ytchong222@google.com>
2024-08-13 19:33:57 +0800

    dbt top 100 overworkers

--- dbt_taxi_project/models/example/daily_max_avg_tips.sql
+++ dbt_taxi_project/models/example/daily_max_avg_tips.sql
@@ -1,12 +1,13 @@
 {{ config(materialized='table') }}
 
 SELECT  FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp) AS tdate,
-  count(taxi_id) as taxi_count,
-  FORMAT('%3.2f',AVG(tips)) AS average_tip,
-  FORMAT('%3.2f',MAX(tips)) AS max_tip
+  count(distinct taxi_id) as no_taxi,
+  count(taxi_id) as taxi_trips,
+  AVG(tips) AS average_tip,
+  MAX(tips) AS max_tip
 FROM
    chicago_taxi_trips.taxi_trips
 where  FORMAT_TIMESTAMP('%Y-%m-%d', trip_start_timestamp) BETWEEN '2023-10-01' AND '2024-01-02'
 group by FORMAT_TIMESTAMP('%Y-%m-%d', trip_end_timestamp) 
 ORDER BY
-  tdate DESC,average_tip DESC 
+  tdate DESC,max_tip DESC 
